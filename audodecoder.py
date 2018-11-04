2d84c5650d4bd05d19cc19e862e2206a2d615f81
root <root@localhost.localdomain>
2018-11-04 19:28:06 +0100

    adding show_all to show chains with errors

--- audodecoder.py
+++ audodecoder.py
@@ -16,7 +16,7 @@ def banner():
  /_/ \_\  \_,_|\__,_|\___/|___/ \___|\__|\___/\__,_|\___| |_|   \n\n\
                     Author: oreos | Twitter: @oreos_ES\n\n")
 
-def decodefunc(message, key, levels, pattern):
+def decodefunc(message, key, levels, pattern, show_all):
     decoder = Decoder()
     decoders = [ 
         "base16",
@@ -33,13 +33,14 @@ def decodefunc(message, key, levels, pattern):
         translated = message
         flow = ""
         for dec in arraydecs:
-            flow = flow+" "+dec+" >"
             translated = decoder.decode(dec, translated, key)
             if translated is None:
+                if show_all is not None and show_all:
+                    print(Fore.RED+" > ".join(arraydecs))
                 break
         if translated is not None:
             if pattern is None or (pattern and pattern in translated):
-                    print(Fore.WHITE+flow+" "+Fore.BLUE+translated)
+                print(Fore.WHITE+" > ".join(arraydecs)+": "+Fore.BLUE+translated)
 
 def main():
     banner()
@@ -47,12 +48,13 @@ def main():
     parser = argparse.ArgumentParser(description='AutoDecoder.py')
     parser.add_argument("-l", "--levels", type=int, help='Number of decoding levels [1-6], default = 2', default=2)
     parser.add_argument("-k", "--key", type=str, help='Key used to decode')
+    parser.add_argument("-s", "--show_all", help='Show chains with errors during decoding', action='store_true')
     parser.add_argument("-p", "--pattern", type=str, help='Search pattern in decoded string')
     requiredNamed = parser.add_argument_group('required named arguments')
     requiredNamed.add_argument('-m', '--message', help='Message to decode', required=True)
     args = parser.parse_args()
     
-    decodefunc(args.message, args.key, args.levels, args.pattern)
+    decodefunc(args.message, args.key, args.levels, args.pattern, args.show_all)
 
 if __name__== "__main__":
     main()
